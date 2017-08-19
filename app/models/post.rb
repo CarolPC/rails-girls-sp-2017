69511875925cb35ffaf2102fea9099aa13af97be
Daiane Baumgartner <Daiane@MacBook-Air-de-Daiane.local>
2017-08-19 18:17:46 -0300

    Finish styling

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-	has_attached_file :picture, styles: { medium: "600x600>", thumb: "100x100>" }
+	has_attached_file :picture, styles: { medium: "600x600>", thumb: "400x400>" }
  	validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
 end
